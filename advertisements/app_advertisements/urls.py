f09ca8feaae4b59a2ec8f8e56c9851fc45b83016
Julia <maole.ru@mail.ru>
2023-08-28 21:54:55 +0400

    commit

--- advertisements/app_advertisements/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import index, top_sellers
-
-urlpatterns = [
-    path("", index, name="index"),
-    path("top-sellers/", top_sellers, name="top-sellers")
-]
